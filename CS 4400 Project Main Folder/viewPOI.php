cf76175acf9219ef221f523c60e169dcb974d8bb
Bethapudi, Sandeep Kumar V <sbethapudi6@gatech.edu>
2017-04-21 18:32:38 -0400

    l

--- CS 4400 Project Main Folder/viewPOI.php
+++ CS 4400 Project Main Folder/viewPOI.php
@@ -1,72 +1,67 @@
 <?php
-// create variable names
-	@ $locName = $_POST['LocName'];
-	@ $city = $_POST['City'];
-	@ $state = $_POST['State'];
-	@ $zip = $_POST['Zip'];
-	@ $checkbox = $_POST['Check'];
-	@ $sDate = $_POST['sDate'];
-	@ $eDate = $_POST['eDate'];
+	// create variable names
+@ $name = $_POST['LocName'];
+@ $city = $_POST['City'];
+@ $state = $_POST['State'];
+@ $zip = $_POST['Zip'];
+@ $check = $_POST['Check'];
+@ $sDate = $_POST['sDate'];
+@ $eDate = $_POST['eDate'];
 
-	$zip = trim($zip);
+$zip = trim($zip);
 
-	if(!strlen($locName) && !strlen($city) && !strlen($state))
+if(!get_magic_quotes_gpc())
+{
+	$name = addslashes($name);
+	$city = addslashes($city);
+	$state = addslashes($state);
+}
+
+@   $db = new mysqli('localhost','root','password','cs4400');
+	$connected = !mysqli_connect_errno();
+	if($connected)
 	{
-		echo 'Please choose Name, city, and state';
-		exit;
-	}
-
-	else
-	{
-		if(!get_magic_quotes_gpc())
+		if(isset($_POST['filter']))
 		{
-			$locName = addslashes($locname);
-			$city = addslashes($city);
-			$state = addslashes($state);
-		}
-
-		@   $db = new mysqli('localhost','root','password','cs4400');
-			$connected = !mysqli_connect_errno();
-			if($connected)
+			if($name || $city || $state || $zip || $check || $sDate || $eDate)
 			{
-				if(isset($_POST['filter']))
-				{
-					if($locName)
-					{
-						$result = $db->query("SELECT `Name` `City` `State` `Zip_Code` `Flag` `Date_Flagged` FROM `poi`");
-	                        while($row = $result -> fetch_assoc())
-	                        {
-	                            $name=$row['Name'];
-                                $city=$row['City'];
-                                $state=$row['State'];
-                                $zip=$row['Zip_Code'];
-                                $flag=$row['Flag'];
-                                $dateFlag=$row['Date_Flagged'];
-
-	                             echo "<table class='table table-striped'>";
-			                        echo "	<thead>
-			                        			<tr>
-								                    <th>Location Name</th>
-								                    <th>City</th>
-								                    <th>State</th>
-								                    <th>Zip Code</th>
-								                    <th>Flagged?</th>
-								                    <th>Date Flagged</th>
-							                    </tr>
-			                            	<thead>
-			                            		</tr>
-					                                <th>name</th>
-								                    <th>city</th>
-								                    <th>state</th>
-								                    <th>zip</th>
-								                    <th>Flag</th>
-								                    <th>dateFlag</th>
-			                            		</tr>
-			                        </thead>";
-			                        echo "<tbody>";
-	                        }
-					}
-				}
+				$result = $db->query("SELECT `Name` `City` `State` `Zip_Code` `Flag` `Date_Flagged` FROM `poi`");
+                    while($row = $result -> fetch_assoc())
+                    {
+                        if($name==$row['Name']||$city==$row['City']||$state==$row['State']||$zip==$row['Zip_Code']||$check==$row['Flag']|| (($dFlag=$row['Date_Flagged'] >= $sDate) && ($dFlag=$row['Date_Flagged'] <= $sDate)))
+                        {
+			                
+			                $name=$row['Name'];
+                            $city=$row['City'];
+                            $state=$row['State'];
+                            $zip=$row['Zip_Code'];
+                            $flag=$row['Flag'];
+                            $dateFlag=$row['Date_Flagged'];
+                            
+                            echo "<table class='table table-striped'>";
+		                        echo "	<thead>
+		                        			<tr>
+							                    <th>Location Name</th>
+							                    <th>City</th>
+							                    <th>State</th>
+							                    <th>Zip Code</th>
+							                    <th>Flagged?</th>
+							                    <th>Date Flagged</th>
+						                    </tr>
+		                            	<thead>
+		                            		</tr>
+				                                <th>name</th>
+							                    <th>city</th>
+							                    <th>state</th>
+							                    <th>zip</th>
+							                    <th>Flag</th>
+							                    <th>dateFlag</th>
+		                            		</tr>
+		                        </thead>";
+		                        echo "<tbody>";
+		                }
+                    }
 			}
-	}			
+		}
+	}		
 ?>
